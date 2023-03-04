d87beb24fe1c1a6abb2b43fd3a89322b7bae0bd9
John Paul Schmidt <TheJohnPaulSchmidt@gmail.com>
2023-03-04 14:10:34 -0600

    Updates

--- src/math.js
+++
@@ -1,8 +0,0 @@
-var foo = 30; // does not create window.foo
-
-const sum = (a, b) => a + b;
-const numbers = [5, 10, 15, 20, 25, foo];
-
-const product = (a,b) => a * b;
-export default product;
-export { numbers };
